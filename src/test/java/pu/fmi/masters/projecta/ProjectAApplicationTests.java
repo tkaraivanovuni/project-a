ec10cf8bbfd97bd723b24614365c16b0bd052bd4
tkaraivanovuni <73016185+tkaraivanovuni@users.noreply.github.com>
2021-05-24 09:46:01 +0300

    Init project A

---
+++ src/test/java/pu/fmi/masters/projecta/ProjectAApplicationTests.java
@@ -0,0 +1,13 @@
+package pu.fmi.masters.projecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
